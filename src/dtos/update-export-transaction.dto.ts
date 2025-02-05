ec07d7b4b57c973b65da3c79a6903bd13ecee44e
Oswald Rodrigues <oswald@logicloop.io>
2025-02-05 11:04:58 +0530

    generated entity fixes

--- src/dtos/update-export-transaction.dto.ts
+++ src/dtos/update-export-transaction.dto.ts
@@ -20,9 +20,9 @@ error: string;
 
 @IsOptional()
 @IsInt()
-exportTemplateIdId: number;
+exportTemplateId: number;
 
 @IsString()
 @IsOptional()
-exportTemplateIdUserKey: string;
+exportTemplateUserKey: string;
 }
