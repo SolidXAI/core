21192acf58ea16453b6869f7c6d1978ff8226c6a
Oswald Rodrigues <oswald@logicloop.io>
2025-02-05 12:37:47 +0530

    export dynamic entity data changes

--- src/services/export-template.service.ts
+++ src/services/export-template.service.ts
@@ -1,22 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { DiscoveryService } from "@nestjs/core";
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 
+import { ConfigService } from '@nestjs/config';
+import { CrudHelperService } from 'src/services/crud-helper.service';
 import { CRUDService } from 'src/services/crud.service';
+import { FileService } from 'src/services/file.service';
+import { MediaStorageProviderMetadataService } from 'src/services/media-storage-provider-metadata.service';
+import { MediaService } from 'src/services/media.service';
 import { ModelMetadataService } from 'src/services/model-metadata.service';
 import { ModuleMetadataService } from 'src/services/module-metadata.service';
-import { MediaStorageProviderMetadataService } from 'src/services/media-storage-provider-metadata.service';
-import { ConfigService } from '@nestjs/config';
-import { MediaService } from 'src/services/media.service';
-import { FileService } from 'src/services/file.service';
-import { CrudHelperService } from 'src/services/crud-helper.service';
 
 
+import { CreateExportTransactionDto } from 'src/dtos/create-export-transaction.dto';
 import { ExportTemplate } from '../entities/export-template.entity';
+import { ExportTransactionService } from './export-transaction.service';
 
 @Injectable()
 export class ExportTemplateService extends CRUDService<ExportTemplate>{
+  async startExport(id: number) {
+    // It should load the export template by id
+    // const exportTemplate = this.findOne(id, { })
+
+    // It should create the export transaction entry
+    const exportTransaction: CreateExportTransactionDto =  await this.exportTransactionService.toDto({
+      datetime: new Date(),
+      status: 'started',
+      exportTemplateId: id,
+    });
+    const exportTransactionEntity = await this.exportTransactionService.create(exportTransaction);
+
+    // Trigger the export process
+    await this.exportTransactionService.triggerExport(exportTransactionEntity.id);
+
+    // It should return the export transaction id
+    return exportTransaction;
+  }
+
   constructor(
     readonly modelMetadataService: ModelMetadataService,
     readonly moduleMetadataService: ModuleMetadataService,
@@ -30,6 +51,7 @@ export class ExportTemplateService extends CRUDService<ExportTemplate>{
     readonly entityManager: EntityManager,
     @InjectRepository(ExportTemplate, 'default')
     readonly repo: Repository<ExportTemplate>,
+    readonly exportTransactionService: ExportTransactionService,
  ) {
    super(modelMetadataService, moduleMetadataService, mediaStorageProviderService, configService, fileService, mediaService, discoveryService, crudHelperService,entityManager, repo, 'exportTemplate', 'solid-core');
  }
--- src/services/export-transaction.service.ts
+++ src/services/export-transaction.service.ts
@@ -1,22 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
+import { Injectable, Logger } from '@nestjs/common';
 import { DiscoveryService } from "@nestjs/core";
+import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 
+import { ConfigService } from '@nestjs/config';
+import { CrudHelperService } from 'src/services/crud-helper.service';
 import { CRUDService } from 'src/services/crud.service';
+import { FileService } from 'src/services/file.service';
+import { MediaStorageProviderMetadataService } from 'src/services/media-storage-provider-metadata.service';
+import { MediaService } from 'src/services/media.service';
 import { ModelMetadataService } from 'src/services/model-metadata.service';
 import { ModuleMetadataService } from 'src/services/module-metadata.service';
-import { MediaStorageProviderMetadataService } from 'src/services/media-storage-provider-metadata.service';
-import { ConfigService } from '@nestjs/config';
-import { MediaService } from 'src/services/media.service';
-import { FileService } from 'src/services/file.service';
-import { CrudHelperService } from 'src/services/crud-helper.service';
 
 
+import { classify } from '@angular-devkit/core/src/utils/strings';
+import { validate } from 'class-validator';
+import { CreateExportTransactionDto } from 'src/dtos/create-export-transaction.dto';
 import { ExportTransaction } from '../entities/export-transaction.entity';
+import { SolidIntrospectService } from './solid-introspect.service';
 
 @Injectable()
-export class ExportTransactionService extends CRUDService<ExportTransaction>{
+export class ExportTransactionService extends CRUDService<ExportTransaction> {
+  private logger = new Logger(ExportTransactionService.name);
+  async triggerExport(exportTransactionId: number) {
+    // Load the export transaction entry
+    const exportTransaction = await this.repo.findOne({
+        where: { id: exportTransactionId },
+        relations: { exportTemplate: { modelMetadata: true } },
+      }
+    );
+
+    // This method will fetch all the columns from the export template & write them to a file
+    // Get the columns which need to be exported & the model id
+    // const modelId = exportTransaction.exportTemplate.modelMetadata;
+    const fields = exportTransaction.exportTemplate.fields;
+
+    // Get the appropriate service for the model by trying to fetch a model service matching a particular name
+    const modelName = exportTransaction.exportTemplate.modelMetadata.singularName;
+    const modelService = this.introspectService.getProvider(`${classify(modelName)}Service`);
+
+
+    // Call the appropriate service method to fetch the data
+    // this.logger.log(`Fetching data for model ${modelName}`);
+    // @ts-ignore
+    const data = await modelService.instance.find({});
+    this.logger.log(`Fetched ${data.length} records for model ${modelName}`);
+
+    // This file will managed using the file storage provider
+    // return data;
+  }
+
   constructor(
     readonly modelMetadataService: ModelMetadataService,
     readonly moduleMetadataService: ModuleMetadataService,
@@ -30,7 +63,19 @@ export class ExportTransactionService extends CRUDService<ExportTransaction>{
     readonly entityManager: EntityManager,
     @InjectRepository(ExportTransaction, 'default')
     readonly repo: Repository<ExportTransaction>,
- ) {
-   super(modelMetadataService, moduleMetadataService, mediaStorageProviderService, configService, fileService, mediaService, discoveryService, crudHelperService,entityManager, repo, 'exportTransaction', 'solid-core');
- }
+    readonly introspectService: SolidIntrospectService,
+
+  ) {
+    super(modelMetadataService, moduleMetadataService, mediaStorageProviderService, configService, fileService, mediaService, discoveryService, crudHelperService, entityManager, repo, 'exportTransaction', 'solid-core');
+  }
+
+  async toDto(data: Partial<CreateExportTransactionDto>): Promise<CreateExportTransactionDto> {
+    const dto = new CreateExportTransactionDto(data);
+    const errors = await validate(dto);
+    if (errors.length > 0) {
+      console.error("Validation failed:", errors);
+      return null;
+    }
+    return dto;
+  }
 }
