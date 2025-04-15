d07bfdf10bd937c121f4f8324791674d20e20482
Jenendar <jenendar.logicloop@gmail.com>
2025-04-15 11:03:21 +0530

    formating

--- src/dtos/update-list-of-values.dto.ts
+++ src/dtos/update-list-of-values.dto.ts
@@ -1,41 +1,36 @@
 import { IsInt,IsOptional, IsString, IsNotEmpty, IsBoolean } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateListOfValuesDto {
     @IsOptional()
     @IsInt()
     id: number;
-
-@IsNotEmpty()
-@IsOptional()
-@IsString()
-@ApiProperty()
-type: string;
-
-@IsNotEmpty()
-@IsOptional()
-@IsString()
-@ApiProperty()
-value: string;
-
-@IsNotEmpty()
-@IsOptional()
-@IsString()
-@ApiProperty()
-display: string;
-
-@IsNotEmpty()
-@IsOptional()
-@IsString()
-@ApiProperty()
-description: string;
-
-@IsOptional()
-@IsBoolean()
-@ApiProperty()
-default: boolean;
-
-@IsOptional()
-@IsInt()
-@ApiProperty()
-sequence: number;
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    @ApiProperty()
+    type: string;
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    @ApiProperty()
+    value: string;
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    @ApiProperty()
+    display: string;
+    @IsNotEmpty()
+    @IsOptional()
+    @IsString()
+    @ApiProperty()
+    description: string;
+    @IsOptional()
+    @IsBoolean()
+    @ApiProperty()
+    default: boolean;
+    @IsOptional()
+    @IsInt()
+    @ApiProperty()
+    sequence: number;
 }
