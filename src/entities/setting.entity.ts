b9690d3f4765c3411ece7c60a4ee0af9b24bb979
Rajesh Chityal <rajesh.chityal@logicloop.io>
2025-04-20 23:23:55 +0530

    setting entity changes

--- src/entities/setting.entity.ts
+++ src/entities/setting.entity.ts
@@ -8,32 +8,4 @@ export class Setting extends CommonEntity {
     keys: string;
     @Column({ type: "varchar", nullable: true })
     values: string;
-    @Column({ type: "varchar", nullable: true })
-    authPagesLayout: string;
-    @Column({ type: "varchar", nullable: true })
-    authPagesTheme: string;
-    @Column({ type: "varchar", nullable: true })
-    appTitle: string;
-    @Column({ type: "varchar", nullable: true })
-    appDescription: string;
-    @Column({ type: "varchar", nullable: true })
-    appTnc: string;
-    @Column({ type: "varchar", nullable: true })
-    appPrivacyPolicy: string;
-    @Column({ type: "boolean", nullable: true, default: false })
-    iamAllowPublicRegistration: boolean = false;
-    @Column({ type: "boolean", nullable: true, default: false })
-    iamPasswordRegistrationEnabled: boolean = false;
-    @Column({ type: "boolean", nullable: true, default: false })
-    iamPasswordLessRegistrationEnabled: boolean = false;
-    @Column({ type: "boolean", nullable: true, default: false })
-    iamActivateUserOnRegistration: boolean = false;
-    @Column({ type: "varchar", nullable: true })
-    iamDefaultRole: string;
-    @Column({ type: "boolean", nullable: true, default: false })
-    iamGoogleOAuthEnabled: boolean = false;
-    @Column({ type: "boolean", nullable: true, default: false })
-    shouldQueueEmails: boolean = false;
-    @Column({ type: "boolean", nullable: true, default: false })
-    shouldQueueSms: boolean = false;
 }
