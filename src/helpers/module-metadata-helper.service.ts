9f76bf42d9a5899db4b354bb1b9bd45eb7c8bffa
sundaram <sundaram.gupta@logicloop.io>
2025-05-23 11:17:38 +0530

    filepath changed in moduleMetadata

--- src/helpers/module-metadata-helper.service.ts
+++ src/helpers/module-metadata-helper.service.ts
@@ -2,6 +2,7 @@ import { Injectable, ModuleMetadata } from "@nestjs/common";
 import * as fs from 'fs/promises'; // Use the Promise-based version of fs for async/await
 import * as path from 'path'; // To handle file paths
 import { ModuleMetadataConfiguration } from "src/interfaces";
+
 @Injectable()
 export class ModuleMetadataHelperService {
     // async getModuleMetadataConfig(moduleName: string): Promise<ModuleMetadata> {
@@ -16,13 +17,13 @@ export class ModuleMetadataHelperService {
     }
 
     getModuleMetadataFilePath(moduleName: string): string {
-        // if (moduleName === `${process.env.DEFAULT_MODULE_NAME}`) {
-        //     return `src/seeders/seed-data/${moduleName}-metadata.json`
-        // }else {
+        if (moduleName === `${process.env.DEFAULT_MODULE_NAME}`) {
+            return `src/seeders/seed-data/${moduleName}-metadata.json`
+        }else {
             //return `module-metadata/${dasherize(moduleName)}/${dasherize(moduleName)}-metadata.json;`
             const folderPath = path.resolve(process.cwd(), 'module-metadata', moduleName);
             const filePath = path.join(folderPath, `${moduleName}-metadata.json`);
             return filePath;
-        //}   
+        }   
     }
 }
