3d19d7eb2284494091a2851f1c1e5dc70cb7d037
Harish Patel <harish@logicloop.io>
2025-06-04 14:25:27 +0100

    api controller tags modified

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -34,21 +34,21 @@ export class TestController {
     return { filename: file.originalname };
   }
 
-  @Public()
-  @Post('seed')
-  async seedData(@Body() seedData: any) {
-    const seeder = this.solidRegistry
-      .getSeeders()
-      .filter((seeder) => seeder.name === seedData.seeder)
-      .map((seeder) => seeder.instance)
-      .pop();
-    if (!seeder) {
-      this.logger.error(`Seeder service ${seedData.seeder} not found. Does your service have a seed() method?`);
-      return;
-    }
-    this.logger.log(`Running the seed() method for seeder :${seeder.constructor.name}`);
-    await seeder.seed();
-    return { message: `seed data for ${seedData.seeder}` };
-  }
+  // @Public()
+  // @Post('seed')
+  // async seedData(@Body() seedData: any) {
+  //   const seeder = this.solidRegistry
+  //     .getSeeders()
+  //     .filter((seeder) => seeder.name === seedData.seeder)
+  //     .map((seeder) => seeder.instance)
+  //     .pop();
+  //   if (!seeder) {
+  //     this.logger.error(`Seeder service ${seedData.seeder} not found. Does your service have a seed() method?`);
+  //     return;
+  //   }
+  //   this.logger.log(`Running the seed() method for seeder :${seeder.constructor.name}`);
+  //   await seeder.seed();
+  //   return { message: `seed data for ${seedData.seeder}` };
+  // }
 
 }
