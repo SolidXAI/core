8235bea52b0c49adc9e0dad64457ae8badeec88a
Oswald Rodrigues <oswald@logicloop.io>
2025-08-29 09:42:31 +0530

    Added support for field paths/expressions in concat entity field provider

--- src/services/computed-fields/entity/concat-entity-computed-field-provider.service.ts
+++ src/services/computed-fields/entity/concat-entity-computed-field-provider.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { kebabCase } from "lodash";
+import { kebabCase, get } from "lodash";
 import { ComputedFieldProvider } from "src/decorators/computed-field-provider.decorator";
 import { CommonEntity } from "src/entities/common.entity";
 import { ComputedFieldMetadata } from "src/helpers/solid-registry";
@@ -24,28 +24,38 @@ export class ConcatEntityComputedFieldProvider<T extends CommonEntity> implement
         return "Computed field provider used to create fields whose value is a concatenation of other fields in the same model.";
     }
 
-    async preComputeValue(triggerEntity: T, computedFieldMetadata: ComputedFieldMetadata<ConcatComputedFieldContext>){
+    async preComputeValue(triggerEntity: T, computedFieldMetadata: ComputedFieldMetadata<ConcatComputedFieldContext>) {
         const { computedFieldValueProviderCtxt } = computedFieldMetadata;
-        const separator = computedFieldValueProviderCtxt.separator || ' '; // Default to space if no separator is provided
-        const fields = computedFieldValueProviderCtxt.fields || [];
-        const slugify = computedFieldValueProviderCtxt.slugify || false;
+        const separator = computedFieldValueProviderCtxt.separator ?? ' ';
+        const fields: string[] = computedFieldValueProviderCtxt.fields ?? [];
+        const slugify = computedFieldValueProviderCtxt.slugify ?? false;
 
-        let concatenatedString = '';
-        for (let i = 0; i < fields.length; i++) {
-            const field = fields[i];
+        const parts: string[] = [];
 
-            // if slugify then each field val to be converted to a slug before concatenation
-            let fieldVal = triggerEntity[field];
-            if (slugify && typeof fieldVal === 'string') {
-                fieldVal = kebabCase(fieldVal);
+        for (const fieldExpr of fields) {
+            let val = get(triggerEntity as any, fieldExpr);
+
+            // normalize to string (skip null/undefined)
+            if (val == null) continue;
+
+            if (typeof val !== 'string') {
+                val = String(val);
             }
 
-            if (concatenatedString) {
-                concatenatedString += separator;
+            if (slugify) {
+                val = kebabCase(val);
             }
-            concatenatedString += fieldVal;
+
+            // ignore empty strings so you don't get stray separators
+            if (val.length > 0) {
+                parts.push(val);
+            }
         }
-        triggerEntity[computedFieldMetadata.fieldName] = concatenatedString; //This set the computed value on the entity, since for pre-compute, entity and triggerEntity are the same
+
+        const concatenatedString = parts.join(separator);
+
+        // set the computed field on the entity
+        (triggerEntity as any)[computedFieldMetadata.fieldName] = concatenatedString;
     }
 
 }
