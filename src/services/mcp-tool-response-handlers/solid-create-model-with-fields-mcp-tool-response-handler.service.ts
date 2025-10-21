4bbb22d1f13c11fb543a0afb77a5a9c2103921b7
Oswald Rodrigues <oswald@logicloop.io>
2025-10-21 13:53:51 +0530

    create model shape changes

--- src/services/mcp-tool-response-handlers/solid-create-model-with-fields-mcp-tool-response-handler.service.ts
+++ src/services/mcp-tool-response-handlers/solid-create-model-with-fields-mcp-tool-response-handler.service.ts
@@ -22,17 +22,18 @@ export class SolidCreateModelWithFieldsMcpToolResponseHandler implements IMcpToo
         const escapedMessage = aiInteraction.message.replace(/\\'/g, "'");
         const aiResponseMessage = JSON.parse(escapedMessage);
 
-        const { moduleUserKey, ...modelSchema } = aiResponseMessage;
+        const { generationStatus, instructions, data } = aiResponseMessage;
+        const { moduleUserKey, schema } = data;
         const moduleMetadata = await this.moduleMetadataService.findOneByUserKey(moduleUserKey);
         if (!moduleMetadata) {
             throw new Error(`Module with user key ${moduleUserKey} not found.`);
         }
-        modelSchema['moduleId'] = moduleMetadata.id;
+        schema['moduleId'] = moduleMetadata.id;
 
         // TODO: Validate if another model with same name exists, if it does then raise an error...
 
         // This creates the module-metadata.json file....
-        const modelObj = await this.modelMetadataService.create(modelSchema as CreateModelMetadataDto);
+        const modelObj = await this.modelMetadataService.create(schema as CreateModelMetadataDto);
 
         // Now we need to run solid seed & then solid refresh-model --name <module-name>
         await this.modelMetadataService.handleGenerateCode({ modelId: modelObj.id });
