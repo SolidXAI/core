ff477ad015a5c0ef78a7f65522062e5e04649472
Oswald Rodrigues <oswald@logicloop.io>
2025-11-05 16:06:18 +0530

    controller handler response changes

--- src/solid-core.module.ts
+++ src/solid-core.module.ts
@@ -298,6 +298,7 @@ import { SolidTsMorphService } from './services/solid-ts-morph.service';
 import { SolidAddVariableToDashboardMcpHandler } from './services/genai/mcp-handlers/solid-add-variable-to-dashboard-mcp-handler.service';
 import { SolidAddQuestionToDashboardMcpHandler } from './services/genai/mcp-handlers/solid-add-question-to-dashboard-mcp-handler.service';
 import { SolidAddCustomServiceMethodMcpHandler } from './services/genai/mcp-handlers/solid-add-custom-service-method-mcp-handler.service';
+import { SolidAddControllerHandlerMcpHandler } from './services/genai/mcp-handlers/solid-add-controller-handler-method-mcp-handler.service';
 
 
 @Global()
@@ -645,6 +646,7 @@ import { SolidAddCustomServiceMethodMcpHandler } from './services/genai/mcp-hand
     SolidAddQuestionToDashboardMcpHandler,
 
     SolidAddCustomServiceMethodMcpHandler,
+    SolidAddControllerHandlerMcpHandler,
     SolidTsMorphService,
 
     ViewMetadataRepository,
