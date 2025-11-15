07e988ab6fca82f77af5cc7fb9f41dd7fb2dcdb8
Oswald Rodrigues <oswald@logicloop.io>
2025-11-15 11:40:12 +0530

    formatted

--- src/controllers/sms-template.controller.ts
+++ src/controllers/sms-template.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
-import { Public } from 'src/decorators/public.decorator';
 import { CreateSmsTemplateDto } from '../dtos/create-sms-template.dto';
 import { UpdateSmsTemplateDto } from '../dtos/update-sms-template.dto';
 import { SmsTemplateService } from '../services/sms-template.service';
@@ -14,58 +13,58 @@ import { SmsTemplateService } from '../services/sms-template.service';
 export class SmsTemplateController {
   constructor(private readonly service: SmsTemplateService) { }
 
-   @ApiBearerAuth("jwt")
-    @Post()
-    @UseInterceptors(AnyFilesInterceptor())
-    create(@Body() createDto: CreateSmsTemplateDto, @UploadedFiles() files: Array<Express.Multer.File>) {
-      return this.service.create(createDto, files);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @Post('/bulk')
-    @UseInterceptors(AnyFilesInterceptor())
-    insertMany(@Body() createDtos: CreateSmsTemplateDto[], @UploadedFiles() filesArray: Express.Multer.File[][] = []) {
-      return this.service.insertMany(createDtos, filesArray);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @Put(':id')
-    @UseInterceptors(AnyFilesInterceptor())
-    update(@Param('id') id: number, @Body() updateDto: UpdateSmsTemplateDto, @UploadedFiles() files: Array<Express.Multer.File>) {
-      return this.service.update(id, updateDto, files);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @ApiQuery({ name: 'showSoftDeleted', required: false, type: Boolean })
-    @ApiQuery({ name: 'showOnlySoftDeleted', required: false, type: Boolean })
-    @ApiQuery({ name: 'limit', required: false, type: Number })
-    @ApiQuery({ name: 'offset', required: false, type: Number })
-    @ApiQuery({ name: 'fields', required: false, type: Array })
-    @ApiQuery({ name: 'sort', required: false, type: Array })
-    @ApiQuery({ name: 'groupBy', required: false, type: Array })
-    @ApiQuery({ name: 'populate', required: false, type: Array })
-    @ApiQuery({ name: 'populateMedia', required: false, type: Array })
-    @ApiQuery({ name: 'filters', required: false, type: Array })
-    @Get()
-    async findMany(@Query() query: any) {
-      return this.service.find(query);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @Get(':id')
-    async findOne(@Param('id') id: string, @Query() query: any) {
-      return this.service.findOne(+id, query);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @Delete('/bulk')
-    async deleteMany(@Body() ids: number[]) {
-      return this.service.deleteMany(ids);
-    }
-  
-    @ApiBearerAuth("jwt")
-    @Delete(':id')
-    async delete(@Param('id') id: number) {
-      return this.service.delete(id);
-    }
+  @ApiBearerAuth("jwt")
+  @Post()
+  @UseInterceptors(AnyFilesInterceptor()) 
+  create(@Body() createDto: CreateSmsTemplateDto, @UploadedFiles() files: Array<Express.Multer.File>) {
+    return this.service.create(createDto, files);
+  }
+
+  @ApiBearerAuth("jwt")
+  @Post('/bulk')
+  @UseInterceptors(AnyFilesInterceptor())
+  insertMany(@Body() createDtos: CreateSmsTemplateDto[], @UploadedFiles() filesArray: Express.Multer.File[][] = []) {
+    return this.service.insertMany(createDtos, filesArray);
+  }
+
+  @ApiBearerAuth("jwt")
+  @Put(':id')
+  @UseInterceptors(AnyFilesInterceptor())
+  update(@Param('id') id: number, @Body() updateDto: UpdateSmsTemplateDto, @UploadedFiles() files: Array<Express.Multer.File>) {
+    return this.service.update(id, updateDto, files);
+  }
+
+  @ApiBearerAuth("jwt")
+  @ApiQuery({ name: 'showSoftDeleted', required: false, type: Boolean })
+  @ApiQuery({ name: 'showOnlySoftDeleted', required: false, type: Boolean })
+  @ApiQuery({ name: 'limit', required: false, type: Number })
+  @ApiQuery({ name: 'offset', required: false, type: Number })
+  @ApiQuery({ name: 'fields', required: false, type: Array })
+  @ApiQuery({ name: 'sort', required: false, type: Array })
+  @ApiQuery({ name: 'groupBy', required: false, type: Array })
+  @ApiQuery({ name: 'populate', required: false, type: Array })
+  @ApiQuery({ name: 'populateMedia', required: false, type: Array })
+  @ApiQuery({ name: 'filters', required: false, type: Array })
+  @Get()
+  async findMany(@Query() query: any) {
+    return this.service.find(query);
+  }
+
+  @ApiBearerAuth("jwt")
+  @Get(':id')
+  async findOne(@Param('id') id: string, @Query() query: any) {
+    return this.service.findOne(+id, query);
+  }
+
+  @ApiBearerAuth("jwt")
+  @Delete('/bulk')
+  async deleteMany(@Body() ids: number[]) {
+    return this.service.deleteMany(ids);
+  }
+
+  @ApiBearerAuth("jwt")
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return this.service.delete(id);
+  }
 }
