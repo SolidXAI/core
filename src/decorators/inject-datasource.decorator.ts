93a1071725f2e96a61bea10d3452938a040e4830
Oswald Rodrigues <oswald@logicloop.io>
2025-11-28 15:14:44 +0530

    Revert "add inject data source decorator"
    This reverts commit 9007f301edcc61fdb313d010f584af2fade8cf52.

--- src/decorators/inject-datasource.decorator.ts
+++
@@ -1,7 +0,0 @@
-// inject-datasource.decorator.ts
-import { Inject } from '@nestjs/common';
-import { getDataSourceToken } from '@nestjs/typeorm';
-
-export function InjectDataSource(connectionName?: string): ParameterDecorator {
-  return Inject(getDataSourceToken(connectionName));
-}
--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,6 @@ export * from './decorators/error-codes-provider.decorator'
 export * from './decorators/solid-database-module.decorator'
 export * from './decorators/solid-service.decorator'
 export * from './decorators/mail-provider.decorator'
-export * from './decorators/disallow-in-production.decorator'
-export * from './decorators/inject-datasource.decorator'
 
 export * from './dtos/post-chatter-message.dto'
 export * from './dtos/basic-filters.dto'
@@ -139,6 +137,7 @@ export * from './entities/dashboard-question-sql-dataset-config.entity'
 export * from './entities/ai-interaction.entity'
 
 export * from './enums/auth-type.enum'
+export * from './decorators/disallow-in-production.decorator'
 
 export * from './filters/http-exception.filter'
 
