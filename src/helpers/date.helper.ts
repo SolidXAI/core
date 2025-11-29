ac832fb547072774fed30eeab8034af944c3ae88
Harish Patel <harish@logicloop.io>
2025-11-29 08:59:33 +0000

    bug fix around date time fields while importing data

--- src/helpers/date.helper.ts
+++ src/helpers/date.helper.ts
@@ -2,12 +2,20 @@ import dayjs from 'dayjs';
 
 
 export function parseFlexibleDate(value: string): Date | null {
-    if (!value) return null;
-    const formats = ['DD-MM-YYYY', 'YYYY-MM-DD'];
-    const parsed = dayjs(value, formats, true); // true = strict parsing
-    if (!parsed.isValid()) {
-      return null;
-    }
-    return parsed.toDate();
+  if (!value) return null;
+
+  const formats = [
+    'DD-MM-YYYY',
+    'YYYY-MM-DD',
+    'YYYY-MM-DD HH:mm:ss',
+    'YYYY-MM-DDTHH:mm:ss',       // ISO without Z
+    'YYYY-MM-DDTHH:mm:ss.SSS',   // ISO with millis
+    'YYYY-MM-DDTHH:mm:ssZ',      // ISO with timezone
+  ];
+
+  const parsed = dayjs(value, formats, true); // true = strict parsing
+  if (!parsed.isValid()) {
+    return null;
   }
-  
+  return parsed.toDate();
+}
