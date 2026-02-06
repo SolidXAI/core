e254c93ed7c06dd8ffd888eab0f3a272d5f84194
Harish Patel <harish@logicloop.io>
2026-02-06 14:31:31 +0000

    foirmatting canges

--- src/dtos/create-saved-filters.dto.ts
+++ src/dtos/create-saved-filters.dto.ts
@@ -7,38 +7,47 @@ export class CreateSavedFiltersDto {
     @IsJSON()
     @ApiProperty()
     filterQueryJson: any;
+
     @IsNotEmpty()
     @IsString()
     @ApiProperty()
     name: string;
+
     @IsOptional()
     @IsBoolean()
     @ApiProperty()
     isPrivate: boolean = false;
+
     @IsOptional()
     @IsInt()
     @ApiProperty({ description: "This is the user id field" })
     userId: number;
+
     @IsString()
     @IsOptional()
     @ApiProperty({ description: "This is the user id field" })
     userUserKey: string;
+
     @IsOptional()
     @IsInt()
     @ApiProperty({ description: "This is the model related to saved filter." })
     modelId: number;
+
     @IsString()
     @IsOptional()
     @ApiProperty({ description: "This is the model related to saved filter." })
     modelUserKey: string;
+
     @IsOptional()
     @IsInt()
     @ApiProperty({ description: "This is the view related to saved filter." })
     viewId: number;
+
     @IsString()
     @IsOptional()
     @ApiProperty({ description: "This is the view related to saved filter." })
     viewUserKey: string;
+
     @IsString()
     @IsOptional()
     @ApiProperty({ description: "A brief description providing additional context about the saved filter’s purpose or usage within the view." })
