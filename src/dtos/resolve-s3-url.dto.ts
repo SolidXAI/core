5e17ff27bc78146063432d1c61753eec8eb01009
abhishek-pandit <abhishek.pandit@git.com>
2026-02-20 16:58:23 +0530

    feat: simplify S3 URL resolution DTO, and remove unused S3 resolution logic.

--- src/dtos/resolve-s3-url.dto.ts
+++ src/dtos/resolve-s3-url.dto.ts
@@ -2,16 +2,6 @@ import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class ResolveS3UrlDto {
     @IsNotEmpty()
-    modelName: string;
-
-    @IsNotEmpty()
-    fieldName: string;
-
-
-    @IsNotEmpty()
-    fileType?: string;
-
-    @IsNotEmpty()
     s3Key?: string;
 
     @IsOptional()
