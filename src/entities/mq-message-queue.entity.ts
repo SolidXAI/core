f3187d1a9b73cfa8c45a8c4a4b237d9a16d490f3
Harish Patel <harish@logicloop.io>
2026-04-02 15:53:04 +0100

    formatting changes

--- src/entities/mq-message-queue.entity.ts
+++ src/entities/mq-message-queue.entity.ts
@@ -1,14 +1,14 @@
 import { CommonEntity } from "src/entities/common.entity"
-import {Entity, Column, OneToMany} from "typeorm";
+import { Entity, Column, OneToMany } from "typeorm";
 import { MqMessage } from 'src/entities/mq-message.entity'
 @Entity("ss_mq_message_queue")
-export class MqMessageQueue extends CommonEntity{
-@Column({ type: "varchar", unique: true })
-name: string;
+export class MqMessageQueue extends CommonEntity {
+    @Column({ type: "varchar", unique: true })
+    name: string;
 
-@Column({ type: "varchar", nullable: true })
-description: string;
+    @Column({ type: "varchar", nullable: true })
+    description: string;
 
-@OneToMany(() => MqMessage, mqMessage => mqMessage.mqMessageQueue, { cascade: true })
-mqMessages: MqMessage[];
+    @OneToMany(() => MqMessage, mqMessage => mqMessage.mqMessageQueue, { cascade: true })
+    mqMessages: MqMessage[];
 }
