95b54b40b9d6b5ef256e5cbbc6d2224daa43c254
Oswald Rodrigues <oswald@logicloop.io>
2026-04-30 12:01:32 +0530

    handled seed scenario too

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -144,7 +144,7 @@ export class AuthenticationService {
         }
     }
 
-    private static readonly SIGNUP_DTO_KEYS = new Set(['username', 'email', 'password', 'fullName', 'mobile', 'roles']);
+    private static readonly SIGNUP_DTO_KEYS = new Set(['username', 'email', 'password', 'fullName', 'mobile', 'roles', 'forcePasswordChange']);
 
     async signUp(signUpDto: SignUpDto & Record<string, any>, activeUser: ActiveUserData = null): Promise<User> {
         const hasExtensionFields = Object.keys(signUpDto).some(k => !AuthenticationService.SIGNUP_DTO_KEYS.has(k));
